a969845255689a9e2bea19cb35179dad7574e99b
Adarsh Shah <github@shahadarsh.com>
2024-03-26 23:04:11 -0700

    bump to 0.20.3 cli version

--- tembo-cli.rb
+++ tembo-cli.rb
@@ -1,19 +1,19 @@
 class TemboCli < Formula
   desc "Tembo CLI allows users to experience Tembo locally, as well as, manage and deploy to Tembo Cloud."
   homepage "https://github.com/tembo-io/tembo/tree/main/tembo-cli"
-  version "0.20.2"
+  version "0.20.3"
   @@filename = nil
-  @@release_version = "2024.3.7"
+  @@release_version = "2024.3.26"
   if OS.mac?
 
     if [[ "${UNAME_MACHINE}" == "arm64" ]]
       @@filename = "tembo-cli-#{version}-aarch64-apple.tar.gz"
       url "https://github.com/tembo-io/tembo/releases/download/#{@@release_version}/#{@@filename}"
-      sha256 "1603eaf9a0c8cba381b364180c34e10b10af0c4093c6fdf3e6ec5594ce71296d"
+      sha256 "815ea7932176eae2dd5796b275893436f875f5574c8a07d2211a909bfd82c90b"
     else
       @@filename = "tembo-cli-#{version}-x86_64-apple.tar.gz"
       url "https://github.com/tembo-io/tembo/releases/download/#{@@release_version}/#{@@filename}"
-      sha256 "22a17be9a6adf18a5e52d9a6174a6a3da0b8107cd12acf3acbf0c8bf057ad144"
+      sha256 "26345023cc419a8b1855cc1f93d885fec5a78dabc56b2971617b00383050db0a"
     end
   elsif OS.linux?
       ohdie "Linux not supported by this formula"
