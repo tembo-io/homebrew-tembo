a8fdadbcde0dc0c2c745924aa9c636505bad9573
coredb-service-user <admin+github@coredb.io>
2024-05-07 16:46:22 +0000

    Update from tembo-io/tembo: 0.20.4

--- tembo-cli.rb
+++ tembo-cli.rb
@@ -3,18 +3,18 @@ class TemboCli < Formula
   homepage "https://github.com/tembo-io/tembo/tree/main/tembo-cli"
   version "0.20.4"
   @@filename = nil
-  @@release_version = "2024.04.25"
+  @@release_version = "2024.05.07"
   if OS.mac?
 
     uname = `/usr/bin/uname -m`
     if uname.strip == "arm64"
       @@filename = "tembo-cli-#{version}-aarch64-apple.tar.gz"
       url "https://github.com/tembo-io/tembo/releases/download/#{@@release_version}/#{@@filename}"
-      sha256 "a47ed147e2fdd4ed38aafb018b07ac60c64165b5e9c0dd363b98dd8def8de0f5"
+      sha256 "8f09686e88a52e58bba575c9d084321469fdda32eaf774b0bb5a339b48b5afce"
     else
       @@filename = "tembo-cli-#{version}-x86_64-apple.tar.gz"
       url "https://github.com/tembo-io/tembo/releases/download/#{@@release_version}/#{@@filename}"
-      sha256 "5e1335b355eea6baf9e8fa35547894d16f3737102927d418ff1594eefcb91129"
+      sha256 "45c905d7d7cc0f0f064e3bbddb1f3fb3afe5ed1178b2d4e346d87c9cbc643d9d"
     end
   elsif OS.linux?
       ohdie "Linux not supported by this formula"
